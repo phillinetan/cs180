44f9f30d4ed84e349927c1050a243609532e433a
phillinetan <elizamei12@yahoo.com.ph>
2017-05-10 22:39:26 +0800

    split data set

--- set.py
+++ set.py
@@ -6,19 +6,20 @@ set2 = open("set2.txt", 'w')
 set3 = open("set3.txt", 'w')
 set4 = open("set4.txt", 'w')
 set5 = open("set5.txt", 'w')
-x = 1
+x = 0
 for line in data:
-	if (x==1):
+	if (x==0):
 		set1.write(line)
-	elif (x==2):
+	elif (x==1):
 		set2.write(line)
-	elif (x==3):
+	elif (x==2):
 		set3.write(line)
-	elif (x==4):
+	elif (x==3):
 		set4.write(line)
-	elif (x==5):
+	elif (x==4):
 		set5.write(line)
-	x = (x+1)%6
+	x = (x+1)%5
+
 
 # for line in data:
 # 	x = random.choice([1,0])
